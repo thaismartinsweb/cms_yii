f5e7a664fab6b976f6d6947452be70497a50bbbb
Thais Martins <thaismartinsweb@gmail.com>
2014-09-10 14:06:25 -0300

    Alteracao de rotas para module

--- protected/config/routes.php
+++ protected/config/routes.php
@@ -6,5 +6,7 @@ $data['<view:about>'] =  array('site/page', 'defaultParams' => array('view' => '
 $data['<controller:\w+>/<id:\d+>'] = '<controller>/view';
 $data['<controller:\w+>/<action:\w+>'] = '<controller>/<action>';
 $data['<controller:\w+>/<action:\w+>/<id:\d+>'] = '<controller>/<action>';
+$data['<module:\w+>/<controller:\w+>/<action:\w+>'] = '<module>/<controller>/<action>';
+$data['<module:\w+>/<controller:\w+>/<action:\w+>/<id:\d+>'] = '<module>/<controller>/<action>';
 
 return $data;
